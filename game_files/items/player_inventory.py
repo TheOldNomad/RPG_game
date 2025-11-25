7fcd57795ee632e9dbeb92361d1d77b8d7007fdc
The_Old_Nomad <imthewhowhenyoucallwhosthere@gmail.com>
2025-11-25 03:09:06 +0200

    inventory turned into wraparound class, will add methods to it

--- game_files/items/player_inventory.py
+++ game_files/items/player_inventory.py
@@ -1,11 +1,14 @@
 from .items import AdvancedPotion, Axe, Potion, Sword
 
-INITIAL_PLAYER_INVENTORY = {
-    "item 1": Sword("Faggot slayer"),
-    "item 2": Axe("N-word crusher"),
-    "item 3": Potion("Baltica 9"),
-    "item 4": AdvancedPotion("Okhota Krepkaya"),
-}
+
+class Inventory:
+    def __init__(self) -> None:
+        self.INITIAL_PLAYER_INVENTORY: dict = {
+            "item 1": Sword("Faggot slayer"),
+            "item 2": Axe("N-word crusher"),
+            "item 3": Potion("Baltica 9"),
+            "item 4": AdvancedPotion("Okhota Krepkaya"),
+        }
 
 
 # Дописать цикл так, чтобы меню стало «интерактивным» — можно открывать инвентарь,
