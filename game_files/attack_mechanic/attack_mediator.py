21508370c9673422ef06e41c1ee21d183dec4162
The_Old_Nomad <imthewhowhenyoucallwhosthere@gmail.com>
2026-05-27 22:14:00 +0300

    Fixed mistakes in AttackMediator, updated damage calculation in DamageDealingEntity

--- game_files/attack_mechanic/attack_mediator.py
+++ game_files/attack_mechanic/attack_mediator.py
@@ -1,12 +1,22 @@
-from game_files.inventories.weapon_and_armor_slots import WeaponAndArmorSlots
-from game_files.entities.damage_dealing_entity import DamageDealingEntity
 from game_files.entities.player import Player
-import random
+from game_files.inventories.weapon_and_armor_slots import WeaponAndArmorSlots
+from game_files.items.armor import Armor
+from game_files.items.weapons import Weapon
+
 
 class AttackMediator:
-    def compute_player_dealt_damage_to_mob(self, player_character: Player, currently_equipped_weapons: WeaponAndArmorSlots, attacked_monster_index: int) -> int:
-        active_weapon_damage_points = currently_equipped_weapons.get_item_parameters
-        damage_dealt_by_player = (player_character.damage + active_weapon_damage_points) * 100%
+    def compute_player_dealt_damage_to_mob(
+        self,
+        player_character: Player,
+        currently_equipped_weapons: WeaponAndArmorSlots,
+        monster_list: list,
+        attacked_monster_index: int,
+        item_to_get_parameters_from: Weapon | Armor | None,
+    ) -> None:
+        active_weapon_damage_points = currently_equipped_weapons.get_item_parameters(item_to_get_parameters_from)
+        if active_weapon_damage_points is None:
+            active_weapon_damage_points = 0
+        damage_dealt_by_player = (player_character.damage + active_weapon_damage_points) / 100
         player_character.deal_damage(monster_list[attacked_monster_index], damage_dealt_by_player)
         if not monster_list[attacked_monster_index].alive:
             monster_list.pop(attacked_monster_index)
--- game_files/entities/damage_dealing_entity.py
+++ game_files/entities/damage_dealing_entity.py
@@ -4,10 +4,10 @@ from game_files.entities.entity import Entity
 
 
 class DamageDealingEntity(Entity):
-    def deal_damage(self, character_taking_damage: Entity) -> None:
+    def deal_damage(self, character_taking_damage: Entity, damage_dealt_to_character: float) -> None:
         if not self.alive:
             return
-        character_taking_damage.health_points = character_taking_damage.health_points - self.damage
+        character_taking_damage.health_points = int(character_taking_damage.health_points * damage_dealt_to_character)
         print(
             character_taking_damage.name
             + " gets hit and screams "
