3c1803b6dfa0bc653579a882d27988e682ef8e31
The_Old_Nomad <imthewhowhenyoucallwhosthere@gmail.com>
2026-07-15 22:04:16 +0300

    Improved code readability, resolved conflicts between versions

--- game_files/mediator_module/mediator_module.py
+++ game_files/mediator_module/mediator_module.py
@@ -1,6 +1,6 @@
-from game_files.inventories.inventory_slot_mediator import InventoryAndActionSlotsMediator
 from game_files.attack_mechanic.attack_mediator import AttackMediator
 from game_files.healing_mechanic.healing_mediator import HealingMediator
+from game_files.inventories.inventory_slot_mediator import InventoryAndActionSlotsMediator
 
 inventory_slot_mediator = InventoryAndActionSlotsMediator()
 attack_mediator = AttackMediator()
